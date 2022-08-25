8cfd253164b254e643afb38e8f57926eb7b467fc
hp <hp@LAPTOP-4PEUEBLA>
2022-08-25 07:41:26 +0530

    second commit

--- JavaDemo/src/com/tnsif/lesson3b/Two.java
+++ JavaDemo/src/com/tnsif/lesson3b/Two.java
@@ -1,6 +1,10 @@
 package com.tnsif.lesson3b;
 
 public class Two {
+	public void dispOutput()
+	{
+		System.out.println("I am Two");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -8,3 +12,4 @@ public class Two {
 	}
 
 }
+
--- JavaDemo/src/com/tnsif/lesson3e/Five.java
+++ JavaDemo/src/com/tnsif/lesson3e/Five.java
@@ -1,10 +1,16 @@
 package com.tnsif.lesson3e;
-
-public class Five {
-
+import com.tnsif.lesson3f.*;
+public class Five{
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 
+		Six obj = new Six();
+		obj.dispOutput();
+		
+		
 	}
 
 }
